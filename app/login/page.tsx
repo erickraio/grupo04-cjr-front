b48b84ca02ccc83d672769bf0aaca708a6c7772a
Gustavo Pires <gmsgu89@gmail.com>
2026-04-30 18:02:02 -0300

    feat: iniciando tela de login

---
+++ app/login/page.tsx
@@ -0,0 +1,16 @@
+export default function Login() {
+    return(
+        <div className="bg-[#fff8e7] min-h-screen flex">
+            <div className="w-1/2 flex flex-col justify-center">
+
+            </div>
+            <div className="w-1/2 flex flex-col justify-center ">
+
+            </div>
+
+         </div>
+    );
+
+
+
+}
