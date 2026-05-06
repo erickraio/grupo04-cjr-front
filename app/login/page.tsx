62f9ecb6916aa2ec4afffa8dee62cf08e710a824
Gustavo Pires <gmsgu89@gmail.com>
2026-05-05 22:10:14 -0300

    feat: Design do olho para mostrar ou ocultar senha

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,27 +1,39 @@
+"use client";
+import { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
 import Image from "next/image";
+
+
+
 export default function Login() {
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
     return (
         //Fundo do site 
         <div className="bg-[#f6f3e4] min-h-screen flex">
             {/*Lado Direito da tela */}
             <div className="w-1/2 flex flex-col justify-start px-24 pt-8">
-            {/*Imagem da logo*/}
-            <Image 
-            src= "/images/logo_stock.io.png"
-            alt = "logo da Stock.io"
-            width={421}
-            height={267}
-            className="-mt-16"
-           
-            />
-            <Image
-            src= "/images/mascote_login.png"
-            alt = "mascote"
-            width ={512.5516357421875}
-            height={1118.5}
-            
+                {/*Imagem da logo*/}
+                <Image
+                    src="/images/logo_stock.io.png"
+                    alt="logo da Stock.io"
+                    width={421}
+                    height={267}
+                    className="-mt-16"
 
-            />
+                />
+                <Image
+                    src="/images/mascote_login.png"
+                    alt="mascote"
+                    width={512.5516357421875}
+                    height={1118.5}
+
+
+                />
 
 
 
@@ -32,23 +44,36 @@ export default function Login() {
                 <div className="bg-[#171918] p-10 rounded-[3rem] w-[654px] h-[1068px]   mt-24 flex  flex-col  items-center">
                     {/*Bloco para o formulário */}
                     <div className="bg-[#171918] p-8 w-[504px] h-[402px]  flex flex-col justify-center ">
-                    <h2 className="text-white text-3xl font-extrabold text-center mb-8 uppercase tracking-wide">
-                        Bem vindo de volta!
-                    </h2>
-                    {/*barra email*/}
-                    <input
-                        className=" bg-[#f6f3e4] text-gray-900 px-6 py-3 rounded-full mb-4 outline-none"
-                        type="email"
-                        placeholder="Email"
-                    />
-                    {/*barra senha*/}
-                    <div className="relative w-full">
+                        <h2 className="text-white text-3xl font-extrabold text-center mb-8 uppercase tracking-wide">
+                            Bem vindo de volta!
+                        </h2>
+                        {/*barra email*/}
                         <input
-                            type="password"
-                            placeholder="Senha"
-                            className="bg-[#f6f3e4] text-gray-900 placeholder-gray-400 px-6 py-3 rounded-full w-full outline-none"
+                            className=" bg-[#f6f3e4] text-gray-900 px-6 py-3 rounded-full mb-4 outline-none"
+                            type="email"
+                            placeholder="Email"
                         />
-                    </div>
+                        {/*barra senha*/}
+
+                        <div className="relative w-full">
+                            <input
+                                type={showPassword ? "text" : "password"} 
+                                placeholder="Senha"
+                                className="bg-[#f6f3e4] text-gray-900 placeholder-gray-400 px-6 py-3 rounded-full w-full outline-none pr-12"
+                            />
+                            <button
+                                type="button"
+                                onClick={togglePasswordVisibility}
+                                className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700 transition-colors"
+                            >
+                                {showPassword ? (
+                                    <EyeOff size={20} /> //  olho fechado
+                                ) : (
+                                    <Eye size={20} />    //  olho aberto
+                                )}
+                            </button>
+                        </div>
+                    
                     {/*Link esqueceu a senha*/}
                     <div className="text-center mt-8 mb-8">
                         <a href="#" className="text-[#ffffff]  text-sm underline decoration-gray-500 hover:text-gray-500 ">
@@ -65,15 +90,15 @@ export default function Login() {
                     <p className="text-white text-sm">Não possui uma conta?
                         <a className="text-[#6A38F3] text-sm font-bold hover:underline"> Cadastre-se</a>
                     </p>
-
-                   </div>
-
+                 </div>
                 </div>
-
 
             </div>
 
+
         </div>
+
+        
     );
 
 
