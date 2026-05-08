792524ddc39f742da270c2a43b54058730bdd822
Gustavo Pires <gmsgu89@gmail.com>
2026-05-08 14:22:34 -0300

    fix: mudando porta do baseURL

--- app/services/api.ts
+++ app/services/api.ts
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'http://localhost:3001/',
 });
 
 export const registerUser = async (userData: any) => {
