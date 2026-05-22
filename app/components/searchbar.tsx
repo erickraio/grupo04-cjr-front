4fd6727e9e981db38c44d319577db842d5fa08ef
erickraio <erickleandroabade.03@gmail.com>
2026-05-22 15:21:48 -0300

    feat: pesquisa funfando!!!

--- app/components/searchbar.tsx
+++ app/components/searchbar.tsx
@@ -15,11 +15,11 @@ export default function SearchBar({onSearch}: SearchBarProps) {
         }
     };
     return (
-        <div className="flex items-center bg-white border rounded-lg px-4 py-2 w-full max-w-md">
+        <div className="flex items-center bg-white border rounded-3xl px-4 py-2 w-full max-w-md">
             <input
                 type="text"
                 placeholder="Procurar por..."
-                value="query"
+                value={query}
                 onChange={(e) => setQuery(e.target.value)}
                 onKeyDown={handleKeyDown}
                 className="flex outline-none text-black bg-transparent text-sm w-full"
@@ -28,8 +28,8 @@ export default function SearchBar({onSearch}: SearchBarProps) {
                 onClick={() => onSearch && onSearch(query)}
                 className="cursor-pointer p-1"
             >
-                <Search size={10} />
+                <Search size={20} color="#6a38f3" />
             </button>
-        </div>
+        </div>  
     )
 }
