f68d31324debacba35d5e2f623d46eb20038379e
erickraio <erickleandroabade.03@gmail.com>
2026-05-31 14:06:00 -0300

    refactor: agora o card dos produtos está separado em components.

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import Image from "next/image";
 import Searchbar from "./components/searchbar";
 import axios from "axios";
 import Link from "next/link";
+import CardProdutos from "./components/CardProdutos";
 // ==========================================
 
 export default function Home() {
@@ -89,7 +90,7 @@ export default function Home() {
 
 
   return (
-    <main className="min-h-screen bg-[#F6F5ED]">
+    <main className="min-h-screen bg-[#f6f3e4]">
 
         {/* NAVBAR */}
       <Navbar />
@@ -166,7 +167,7 @@ export default function Home() {
             <div className="flex gap-6 overflow-x-auto pb-6 pt-2 scrollbar-hide">
                 {produtos.map((produto: any) => (
                 <div key={produto.id} className="min-w-[220px] md:min-w-[260px]">
-                <CardProduto data={produto} />
+                <CardProdutos data={produto} />
               </div>
               ))}
             </div>
@@ -208,41 +209,5 @@ export default function Home() {
       </section>
 
     </main>
-  );
-}
-
-// ==========================================
-// COMPONENTE DE CARD DE PRODUTO 
-// ==========================================
-
-function CardProduto({ data }: { data: any }) {
-  // Ajustando para os nomes prováveis do backend (banco de dados)
-  const isDisponivel = data.estoque > 0; // Exemplo: se tem quantidade, está disponível
-
-  const urlImagem = data?.imagens?.[0]?.url_imagem || '/images/steamdeck.png'; // Imagem do produto ou placeholder
-  
-  return (
-     <Link href={`/produto-especifico/${data.id}`}> 
-    <div className="bg-white rounded-3xl p-4 md:p-5 shadow-[0px_4px_15px_rgba(0,0,0,0.02)] flex flex-col relative border border-transparent hover:border-gray-200 transition-all cursor-pointer">
-      
-      {/* Área da Imagem do Produto (Espaço Disponível) */}
-      <div className="relative w-full h-[160px] bg-gray-50 rounded-2xl mb-4 flex items-center justify-center text-gray-300 text-xs mt-4">
-         <Image src={urlImagem} alt={data.nome} fill className="object-contain" />
-      </div>
-
-      {/* Informações de Texto */}
-      <div className="flex flex-col gap-1">
-        <h3 className="text-xl font-bold text-black leading-tight truncate">{data.nome}</h3>
-        
-        <p className="text-xl font-bold text-black flex items-baseline gap-1 mt-1">
-          R$ {data.preco} 
-        </p>
-        
-        <span className={`text-[11px] font-bold mt-1 tracking-wide ${isDisponivel ? 'text-[#B5D400]' : 'text-[#E53E3E]'}`}>
-          {isDisponivel ? 'DISPONÍVEL' : 'INDISPONÍVEL'}
-        </span>
-      </div>
-    </div>
-    </Link>
   );
 }
